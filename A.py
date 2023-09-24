cd5ea6ecce941fdaface4c0fade60d22fcde4e07
Mildam45 <Mike.kotenev@yandex.ru>
2023-09-24 22:13:49 +0300

    A

---
+++ A.py
@@ -0,0 +1,16 @@
+
+n = int(input())
+
+for i in range(n):
+    print("+___", end=" ")
+print()
+for i in range(1, n + 1):
+    print("|" + str(i) + " /", end=" ")
+print()
+for i in range(n):
+    print("|__\\", end=" ")
+print()
+for i in range(n):
+    print("|   ", end=" ")
+print()
+
